d6b53e8c9ede54138494e9fa58ce25b56723e277
pinn99k <minn90311@gmail.com>
2026-08-28 18:57:32 +0900

    success 02 closes #07

--- week2/1. basic/02_array.py
+++ week2/1. basic/02_array.py
@@ -30,7 +30,7 @@
 - 회전 후 위치: (i, j) -> (j, n-1-i)
 - 새로운 배열을 만들어 값을 채워넣으세요
 """
-# 아직 못품
+
 def rotate_matrix_90(matrix):
     """
     2차원 배열을 시계방향으로 90도 회전
@@ -44,13 +44,16 @@ def rotate_matrix_90(matrix):
     n = len(matrix)
     
     # TODO: n x n 크기의 새로운 배열을 생성하세요 (0으로 초기화)
-    rotated = [[0]*n]*n
+    # [[0]*n]*n > 얕은 복사라 제대로 복사되지 않음
+    rotated = [[0] * n for _ in range(n)]
     pass
         
     # TODO: 원본 배열의 각 요소를 회전된 위치에 배치하세요
     # 힌트: (i, j) 위치의 요소는 회전 후 (j, n-1-i) 위치로 이동
     pass
-
+    for i in range(n):
+        for j in range(n):
+            rotated[j][n-1-i] = matrix[i][j]
     
     return rotated
 
