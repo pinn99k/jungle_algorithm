43f82e5278e924532326d4d6a4fb35c74118d3aa
pinn99k <minn90311@gmail.com>
2026-08-29 17:47:42 +0900

    success #23

--- week2/2. advanced/03_priority_queue.py
+++ week2/2. advanced/03_priority_queue.py
@@ -41,13 +41,19 @@ def process_emergency_room(patients):
     
     
     # TODO: 모든 환자를 힙에 추가
+    for h in patients:
+        heapq.heappush(heap, (h[1],h[0]))
     pass
         
     processed = []
     
     # TODO: 힙이 비어있지 않은 동안 반복
+    while heap:
     ## 힙에서 우선순위가 가장 높은 환자 꺼내기
+        person = heapq.heappop(heap)
+        print(f"처리: {person[1]} (우선순위: {person[0]})")
     ## 환자 처리
+        processed.append(person[1])
     pass
         
     return processed
