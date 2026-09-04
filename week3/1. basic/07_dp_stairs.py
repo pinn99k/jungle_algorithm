5d5856e5e827ed627dd6233d9f22b5dfb92a8ea0
pinn99k <minn90311@gmail.com>
2026-09-04 14:24:10 +0900

    success 07
    closes #38

--- week3/1. basic/07_dp_stairs.py
+++ week3/1. basic/07_dp_stairs.py
@@ -29,6 +29,7 @@
 
 DP 문제 풀이 순서:
 1. 부분 문제 정의: dp[i]가 무엇인지 정의
+i까지 오르는 경우의 수
 2. 점화식 도출: dp[i]를 이전 값으로 표현
 3. 초기값 설정: dp[0], dp[1] 등
 4. 계산 순서 결정: 상향식 or 하향식
@@ -46,14 +47,18 @@ def climb_stairs(n):
         n번째 계단까지 오르는 방법의 수
     """
     # TODO: 특별한 경우 처리
-    pass
+    if n < 3:
+        return n
     
     
     # TODO: dp 배열 생성 및 초기화
-    pass
+    dp = [0]*(n+1)
+    dp[1] = 1
+    dp[2] = 2
     
     # TODO: 작은 문제부터 차례로 계산
-    pass
+    for i in range(3, n+1):
+        dp[i] = dp[i-1]+dp[i-2]
     
     return dp[n]
 
