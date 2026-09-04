e9dce2a5fee71acec1fbe29b5dfdbfd082c34c18
pinn99k <minn90311@gmail.com>
2026-09-04 14:42:44 +0900

    success 08
    closes #39

--- week3/1. basic/08_greedy_coin.py
+++ week3/1. basic/08_greedy_coin.py
@@ -44,7 +44,13 @@ def make_change_greedy(change, coins):
     total_coins = 0
     
     # TODO: 각 동전에 대해 반복
-    ## 현재 동전으로 거슬러줄 수 있는 개수 계산    
+    for i in coins:
+        k = change//i # k는 몫
+        if k > 0:
+            total_coins += k
+            result[i] = k
+            change %= i
+    ## 현재 동전으로 거슬러줄 수 있는 개수 계산
     ## 개수가 0보다 크면 결과에 추가
     pass
     
