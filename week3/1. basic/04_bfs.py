51a15f870387f91616b1a0740dc3931bf25203e1
pinn99k <minn90311@gmail.com>
2026-09-04 11:38:17 +0900

    success 04
    closes #35

--- week3/1. basic/04_bfs.py
+++ week3/1. basic/04_bfs.py
@@ -47,13 +47,21 @@ def bfs(graph, start):
     
     # TODO: 큐 생성 및 시작 정점 추가
     ## 방문한 정점 집합
-    pass
-
+    # 0으로 초기화?
+    queue = deque([start])
+    visited.append(start)
     # TODO: 큐가 빌 때까지 반복
+    while queue:
     ## 큐에서 정점 꺼내기
+        now = queue.popleft()
     ## 인접한 정점들 확인
+        for i in graph[now]:
+            # 파이썬은 이게 가능
+            if i not in visited:
+                queue.append(i)
+                visited.append(i)
+        
     ## 방문하지 않은 정점이면 큐에 추가
-    pass
     
     return visited
 
